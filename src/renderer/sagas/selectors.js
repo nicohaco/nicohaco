eb8bd77ec4c88c8d30f7afb7307d56bed62d653a
Yuta Hiroto <git@about-hiroppy.com>
2018-01-25 10:25:42 +0900

    feat: implement VideoPlayer with alwaysOnTop

--- src/renderer/sagas/selectors.js
+++ src/renderer/sagas/selectors.js
@@ -2,10 +2,11 @@
 
 import type { State } from '../types/states';
 
-export const getNico               = (state: State) => state.auth.nicoInstance;
-export const getPlayer             = (state: State) => state.player; // [TODO] rename
-export const getUserData           = (state: State) => state.auth.userData;
-export const getPlaylist           = (state: State) => state.player.playlist;
-export const getCurrentMylist      = (state: State) => state.mylist.mylist;
-export const getCurrentPlaying     = (state: State) => state.player.current;
-export const getCurrentMylistgroup = (state: State) => state.mylist.group;
+export const getNico                   = (state: State) => state.auth.nicoInstance;
+export const getPlayer                 = (state: State) => state.player; // [TODO] rename
+export const getUserData               = (state: State) => state.auth.userData;
+export const getPlaylist               = (state: State) => state.player.playlist;
+export const getCurrentMylist          = (state: State) => state.mylist.mylist;
+export const getCurrentPlaying         = (state: State) => state.player.current;
+export const getCurrentMylistgroup     = (state: State) => state.mylist.group;
+export const getCurrentSubWindowStatus = (state: State) => state.common.subWindow.opened;
