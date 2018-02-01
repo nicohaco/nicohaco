3d377230e935fc94922bdb49190f3ed97754ce14
Yuta Hiroto <git@about-hiroppy.com>
2018-02-02 00:35:08 +0900

    feat(Player): add notification

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -11,6 +11,9 @@ import '!style-loader!css-loader!rc-slider/assets/index.css';
 // $FlowFixMe
 import '!style-loader!css-loader!react-select/dist/react-select.css';
 
+// $FlowFixMe
+import '!style-loader!css-loader!./styles/react-select.css';
+
 // import Analytics from 'electron-google-analytics';
 // const analytics = new Analytics('UA-113205184-1'); // TODO: wip
 const rootEl = document.createElement('div');
